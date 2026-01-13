6e5c2a30f48deaae2e0a1a65dd1739afceeaed7c
mohamed momen <mohamedmomen81296@gmail.com>
2026-01-13 16:54:47 +0200

    edit all reorder end point url

--- Modules/Admin/routes/api.php
+++ Modules/Admin/routes/api.php
@@ -15,7 +15,7 @@ use Modules\Admin\Http\Controllers\UserController;
 Route::get('users', [UserController::class, 'index'])->name('admin.users.index');
 Route::post('users', [UserController::class, 'store'])->name('admin.users.store');
 Route::post('users/bulk-delete', [UserController::class, 'bulkDestroy'])->name('admin.users.bulk_destroy');
-Route::post('reorder/users', [UserController::class, 'updateOrder'])->name('admin.users.reorder');
+Route::post('users/reorder', [UserController::class, 'updateOrder'])->name('admin.users.reorder');
 Route::get('users/{id}', [UserController::class, 'show'])->name('admin.users.show');
 Route::post('users/{id}', [UserController::class, 'update'])->name('admin.users.update');
 Route::delete('users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
@@ -27,11 +27,11 @@ Route::post('users/{id}/direct-permissions/revoke', [DirectPermissionController:
 Route::get('roles', [RoleController::class, 'index'])->name('admin.roles.index');
 Route::post('roles', [RoleController::class, 'store'])->name('admin.roles.store');
 Route::post('roles/bulk-delete', [RoleController::class, 'bulkDestroy'])->name('admin.roles.bulk_destroy');
-Route::post('reorder/roles', [RoleController::class, 'updateOrder'])->name('admin.roles.reorder');
+Route::post('roles/reorder', [RoleController::class, 'updateOrder'])->name('admin.roles.reorder');
 Route::get('roles/{id}', [RoleController::class, 'show'])->name('admin.roles.show');
 Route::post('roles/{id}', [RoleController::class, 'update'])->name('admin.roles.update');
 Route::delete('roles/{id}', [RoleController::class, 'destroy'])->name('admin.roles.destroy');
 
 Route::get('permissions', [PermissionController::class, 'index'])->name('admin.permissions.index');
 Route::get('permissions/{id}', [PermissionController::class, 'show'])->name('admin.permissions.show');
-Route::post('reorder/permissions', [PermissionController::class, 'updateOrder'])->name('admin.permissions.reorder');
+Route::post('permissions/reorder', [PermissionController::class, 'updateOrder'])->name('admin.permissions.reorder');
